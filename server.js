33809f3c29592313c0e61416b81b927463f8156d
Sage Wall <swall@esri.com>
2024-09-25 07:37:32 -0600

    feat: add cors header to certain paths

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ var serveStatic = require("serve-static");
 
 var app = express();
 
-app.use(serveStatic("static", { acceptRanges: false }));
+app.use(serveStatic("static", { acceptRanges: false, setHeaders: setHeaders }));
 
 app.listen(3000);
 console.log("Server started on http://localhost:3000");
+
+function setHeaders(res, path) {
+  if (path.includes("CORS_enabled")) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  }
+}
